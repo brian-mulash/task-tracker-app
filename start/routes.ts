760256d4cabe71af5347db8d132e7d0845a07ae6
“brian-mulash” <brianmulash03@gmail.com>
2023-03-29 14:06:50 +0300

    created adonisjs/auth set-up, user models and migrations, made login and register views and also made some small changes in the app navbar

--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,14 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-
+/* task */
 Route.get('/', 'TasksController.index')
-
 Route.post('/tasks', 'TasksController.store')
-
 Route.patch('/tasks/:id', 'TasksController.update')
-
 Route.delete('/tasks/:id', 'TasksController.delete')
+
+/* user */
+Route.get('/register', 'AuthController.showRegister')
+Route.post('/register', 'AuthController.registerUser')
+
+Route.get('/login', 'AuthController.showLogin')
+Route.post('/login', 'AuthController.loginUser')
